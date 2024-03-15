cd53ba4af223166bd7db25cedfb4bf8e68301db0
RenanStMeira <renan.meira25@gmail.com>
2024-03-15 20:43:18 -0300

    create: mmodel pessoa

---
+++ src/main/java/br/com/dbc/vemser/iShirts/model/Pessoa.java
@@ -0,0 +1,48 @@
+package br.com.dbc.vemser.iShirts.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Table(name = "PESSOA")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PESSOA")
+    @SequenceGenerator(name = "SEQ_PESSOA", sequenceName = "SEQ_PESSOA", allocationSize = 1)
+    @Column(name = "ID_PESSOA")
+    private Integer idPessoa;
+
+    @OneToOne
+    @JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID_USUARIO")
+    private Usuario usuario;
+
+    @Column(name = "NOME", nullable = false)
+    private String nome;
+
+    @Column(name = "SOBRENOME")
+    private String sobrenome;
+
+    @Column(name = "CPF", nullable = false)
+    private String cpf;
+
+    @Column(name = "CELULAR", nullable = false)
+    private String celular;
+
+    @Column(name = "DATA_NASCIMENTO")
+    private Date dataNascimento;
+
+    @Column(name = "PREFERENCIA", nullable = false)
+    private String preferencia;
+
+    @Column(name = "ATIVO", nullable = false)
+    private char ativo;
+
+    @Column(name = "CRIADO")
+    private Date criado;
+
+    @Column(name = "EDITADO")
+    private Date editado;
+
+}
