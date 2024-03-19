cc6fcb8abd91ec13ba4e4d61aba284ad7ad4434f
getwlad <wladmir.rrodrigues@gmail.com>
2024-03-19 14:53:17 -0300

    test: usuario service

---
+++ src/test/java/br/com/dbc/vemser/iShirts/service/mocks/MockUsuario.java
@@ -0,0 +1,55 @@
+package br.com.dbc.vemser.iShirts.service.mocks;
+
+import br.com.dbc.vemser.iShirts.dto.usuario.UsuarioCreateDTO;
+import br.com.dbc.vemser.iShirts.dto.usuario.UsuarioDTO;
+import br.com.dbc.vemser.iShirts.dto.usuario.UsuarioUpdateDTO;
+import br.com.dbc.vemser.iShirts.model.Usuario;
+import br.com.dbc.vemser.iShirts.model.enums.Ativo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MockUsuario {
+    public static Usuario retornarEntity(){
+        Usuario usuario = new Usuario();
+        usuario.setIdUsuario(new Random().nextInt());
+        usuario.setAtivo(Ativo.ATIVO);
+        usuario.setEmail("teste@dbccompany.com.br");
+        usuario.setSenha("senha123");
+        return usuario;
+    }
+    public static List<Usuario> retornarListaUsuarioEntity(){
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(retornarEntity());
+        usuarios.add(retornarEntity());
+        return usuarios;
+    }
+    public static List<UsuarioDTO> retornarListaUsuarioDTO(){
+        List<UsuarioDTO> usuarios = new ArrayList<>();
+        usuarios.add(retornarUsuarioDTO());
+        usuarios.add(retornarUsuarioDTO());
+        return usuarios;
+    }
+    public static UsuarioDTO retornarDTOPorEntity(Usuario usuario){
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(usuario.getIdUsuario());
+        usuarioDTO.setEmail(usuario.getEmail());
+        return usuarioDTO;
+    }
+    public static UsuarioDTO retornarUsuarioDTO(){
+        return retornarDTOPorEntity(retornarEntity());
+    }
+    public static UsuarioCreateDTO retornarUsuarioCreateDTO(){
+        UsuarioCreateDTO usuarioCreateDTO = new UsuarioCreateDTO();
+        usuarioCreateDTO.setEmail("teste@dbccompany.com.br");
+        usuarioCreateDTO.setSenha("senha123");
+        return usuarioCreateDTO;
+    }
+    public static UsuarioUpdateDTO retornarUsuarioUpdate(){
+        UsuarioUpdateDTO usuarioUpdateDTO = new UsuarioUpdateDTO();
+        usuarioUpdateDTO.setEmail("testeatualiza@dbccompany.com.br");
+        usuarioUpdateDTO.setSenha("senha321");
+        return usuarioUpdateDTO;
+    }
+}
