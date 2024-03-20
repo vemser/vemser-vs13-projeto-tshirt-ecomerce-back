2e36e49d905d1efbd70693edcc92c4c641e8a131
RenanStMeira <renan.meira25@gmail.com>
2024-03-20 16:55:17 -0300

    create: interface login

--- src/main/java/br/com/dbc/vemser/iShirts/controller/AuthController.java
+++ src/main/java/br/com/dbc/vemser/iShirts/controller/AuthController.java
@@ -1,6 +1,7 @@
 package br.com.dbc.vemser.iShirts.controller;
 
 
+import br.com.dbc.vemser.iShirts.controller.interfaces.AuthControllerInterface;
 import br.com.dbc.vemser.iShirts.dto.auth.*;
 import br.com.dbc.vemser.iShirts.dto.usuario.*;
 import br.com.dbc.vemser.iShirts.exceptions.RegraDeNegocioException;
@@ -25,7 +26,7 @@ import javax.validation.Valid;
 @Validated
 @Tag(name = "Autenticação", description = "Endpoint de Autenticação")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthControllerInterface {
 
     private final TokenService tokenService;
     public final AuthenticationManager authenticationManager;
