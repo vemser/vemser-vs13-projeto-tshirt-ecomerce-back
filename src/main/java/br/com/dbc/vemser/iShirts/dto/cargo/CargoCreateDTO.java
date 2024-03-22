3bb9ccc3619bb2f607343ad146d785c60d439ac3
Rafael Camilo De Olivera <rafael.camilo21@outlook.com>
2024-03-22 14:18:02 -0300

    feat:cargo-doc

--- src/main/java/br/com/dbc/vemser/iShirts/dto/cargo/CargoCreateDTO.java
+++ src/main/java/br/com/dbc/vemser/iShirts/dto/cargo/CargoCreateDTO.java
@@ -1,10 +1,12 @@
 package br.com.dbc.vemser.iShirts.dto.cargo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 public class CargoCreateDTO {
+    @Schema(description = "Descrição do cargo", required = true)
     @NotNull
     private String descricao;
 }
